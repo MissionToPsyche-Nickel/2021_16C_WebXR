77d6ece04cd9bf7ce8f306213ffa486cd859ce1a
osapkal <osapkal@asu.edu>
2026-03-04 10:59:33 -0700

    Updates

---
+++ precache-manifest.c13d03ae091bed070105f32be65314f3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce184c6a4bb750680c701c05317e6d25",
+    "url": "/2021_16C_WebXR/index.html"
+  },
+  {
+    "revision": "99ced2014f7a1fb01c47",
+    "url": "/2021_16C_WebXR/static/css/main.33d0e61a.chunk.css"
+  },
+  {
+    "revision": "406c764196ad5028e7a4",
+    "url": "/2021_16C_WebXR/static/js/2.90247031.chunk.js"
+  },
+  {
+    "revision": "daf60b6218f4eef6142467cf6d76ac20",
+    "url": "/2021_16C_WebXR/static/js/2.90247031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ced2014f7a1fb01c47",
+    "url": "/2021_16C_WebXR/static/js/main.0689cd9a.chunk.js"
+  },
+  {
+    "revision": "bda327c5409bfe334c91",
+    "url": "/2021_16C_WebXR/static/js/runtime-main.f74b958b.js"
+  },
+  {
+    "revision": "c34048dfa6af0b4aa10ed8d544b19ee4",
+    "url": "/2021_16C_WebXR/static/media/Liangbin.c34048df.png"
+  },
+  {
+    "revision": "64d46ac5e31a74a5e147dfd28ef3b547",
+    "url": "/2021_16C_WebXR/static/media/Menu-AboutPsyche.64d46ac5.png"
+  },
+  {
+    "revision": "a3eb899cee6a16e2c6af11a4dfe6e2da",
+    "url": "/2021_16C_WebXR/static/media/Menu-AboutTeam.a3eb899c.png"
+  },
+  {
+    "revision": "1eb706df1f9884afdafa705947bdd3a2",
+    "url": "/2021_16C_WebXR/static/media/Menu-Asteriod.1eb706df.png"
+  },
+  {
+    "revision": "68d5c3235ba94b6f311a4e7c4ddb38f9",
+    "url": "/2021_16C_WebXR/static/media/MenuButton.68d5c323.svg"
+  },
+  {
+    "revision": "3fc8937de483a26a773a17d58ecb8f4b",
+    "url": "/2021_16C_WebXR/static/media/Psyche-Badge-Mono.3fc8937d.png"
+  },
+  {
+    "revision": "854bbd7ead69da275ebe43920d58fc8b",
+    "url": "/2021_16C_WebXR/static/media/Psyche-Badge.854bbd7e.png"
+  },
+  {
+    "revision": "ea725d2738b43547a1b3bd89ccfe6d0a",
+    "url": "/2021_16C_WebXR/static/media/RetroSpaceCraft.ea725d27.png"
+  },
+  {
+    "revision": "6b8138fce34ecd3503578210e6aa03b6",
+    "url": "/2021_16C_WebXR/static/media/Ryan.6b8138fc.png"
+  },
+  {
+    "revision": "9c5830abcc6f4c70505aa9ea4ccbc153",
+    "url": "/2021_16C_WebXR/static/media/Spacecraft.9c5830ab.glb"
+  },
+  {
+    "revision": "8592cb16687c7a4b90c0ec4efe04e714",
+    "url": "/2021_16C_WebXR/static/media/TexturedAsteroid.8592cb16.glb"
+  },
+  {
+    "revision": "c069603d0440e6ea9b1c8865525e2304",
+    "url": "/2021_16C_WebXR/static/media/Yu.c069603d.png"
+  },
+  {
+    "revision": "4323e80b67f4b903587e05e8eeeb2cbf",
+    "url": "/2021_16C_WebXR/static/media/Zack.4323e80b.png"
+  },
+  {
+    "revision": "ff1d322daa20d811d983e49ee7a0a4a3",
+    "url": "/2021_16C_WebXR/static/media/github.ff1d322d.png"
+  },
+  {
+    "revision": "adb54d029a383fbf502e39ed5f7e35a9",
+    "url": "/2021_16C_WebXR/static/media/orbits-v2.adb54d02.svg"
+  },
+  {
+    "revision": "c924862bf2e6190c2f389c661e0fe19f",
+    "url": "/2021_16C_WebXR/static/media/starmap_background.c924862b.jpg"
+  },
+  {
+    "revision": "d0a6e038a5f055ce396409ef52bea88a",
+    "url": "/2021_16C_WebXR/static/media/trajectory.d0a6e038.svg"
+  }
+]);
